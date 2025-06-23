1eb2332bf747185f106279fe30bdbcdb4dcc86c7
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-06-23 11:15:40 +0530

    Revert "now we will see"
    This reverts commit 80f31c8e05f262eccd3afc1abe61cb0116c31191.

--- Naman/util/keepalive.py
+++ Naman/util/keepalive.py
@@ -13,7 +13,7 @@ async def keep_alive():
             try:
                 async with session.get(URL) as resp:
                     if resp.status != 200:
-                        logging.info(f"⚠️ Ping Error! Status: {resp.status}")
+                        logging.warning(f"⚠️ Ping Error! Status: {resp.status}")
             except Exception as e:
                 logging.error(f"❌ Ping Failed: {e}")
 
