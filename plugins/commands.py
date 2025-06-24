78fbde855d616b2a064ad95ba7cb9cac93688434
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-06-24 15:27:04 +0530

    new 10k

--- plugins/commands.py
+++ plugins/commands.py
@@ -113,7 +113,8 @@ async def start(client, message):
             logger.error("Make sure Bot is admin in Forcesub channel")
             return
         btn = [[
-            InlineKeyboardButton("❆ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ❆", url=invite_link.invite_link)
+            #InlineKeyboardButton("❆ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ❆", url=invite_link.invite_link)
+            InlineKeyboardButton("❆ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ❆", url="https://t.me/+Es37dkfPYIQyYmU1")
         ]]
         if message.command[1] != "subscribe":
             if REQUEST_TO_JOIN_MODE == True:
