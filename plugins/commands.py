033b871c810155bc98bfb80b9268f80538902665
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-07-20 11:01:45 +0530

    new proff

--- plugins/commands.py
+++ plugins/commands.py
@@ -114,7 +114,7 @@ async def start(client, message):
             return
         btn = [[
             #InlineKeyboardButton("❆ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ❆", url=invite_link.invite_link)
-            InlineKeyboardButton("❆ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ❆", url="https://t.me/+_eCpIJmcGs9mNDdl")
+            InlineKeyboardButton("❆ Jᴏɪɴ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ❆", url="https://t.me/+e2PXfYbLo7UwY2Zl")
         ]]
         if message.command[1] != "subscribe":
             if REQUEST_TO_JOIN_MODE == True:
