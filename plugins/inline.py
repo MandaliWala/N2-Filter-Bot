882c11a6af85bb94e7d66fc115b35acc47f5ab47
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-07-30 12:45:26 +0530

    back

--- plugins/inline.py
+++ plugins/inline.py
@@ -48,7 +48,7 @@ async def answer(bot, query):
 
     offset = int(query.offset or 0)
     reply_markup = get_reply_markup(query=string)
-    files, next_offset, total = await get_custom_search_results(
+    files, next_offset, total = await get_search1_results(
                                                   string,
                                                   max_results=6,
                                                   offset=offset)
