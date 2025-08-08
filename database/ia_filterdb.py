2db121e041b3be0683450ee120d719a222eba878
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-08-08 17:39:58 +0530

    lets give it a try

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -1,6 +1,6 @@
 import logging
 from struct import pack
-import re
+import re, asyncio
 import base64
 from pyrogram.file_id import FileId
 from pymongo.errors import DuplicateKeyError
@@ -186,43 +186,36 @@ async def save_file(media):
 #     return files, next_offset, total_results
 
 async def get_search1_results(query, file_type=None, max_results=10, offset=0, filter=False):
-    """For given query return (results, next_offset)"""
+    """For given query return (results, next_offset, total_results)"""
     max_results = int(MAX_B_TN)
     query = query.strip()
-    #if filter:
-        #better ?
-        #query = query.replace(' ', r'(\s|\.|\+|\-|_)')
-        #raw_pattern = r'(\s|_|\-|\.|\+)' + query + r'(\s|_|\-|\.|\+)'
+    
     if not query:
         raw_pattern = '.'
     elif ' ' not in query:
-        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
+        raw_pattern = r'(\b|[\.\+\-_])' + re.escape(query) + r'(\b|[\.\+\-_])'
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+        raw_pattern = r'.*[\s\.\+\-_]'.join(map(re.escape, query.split()))
     
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
-    except:
-        return []
+    except re.error:
+        return [], '', 0
 
     filter = {'file_name': regex}
     
-
-
-    total_results = await Media.count_documents(filter)
-    next_offset = offset + max_results
-
-    if next_offset > total_results:
-        next_offset = ''
-
-    cursor = Media.find(filter)
-    # Sort by recent
-    cursor.sort('$natural', -1)
-    # Slice files according to offset and max results
-    cursor.skip(offset).limit(max_results)
-    # Get list of files
-    files = await cursor.to_list(length=max_results)
-
+    # Parallelize count and fetch operations
+    total_results, files = await asyncio.gather(
+        Media.count_documents(filter),
+        Media.find(filter)
+            .sort('$natural', -1)
+            .skip(offset)
+            .limit(max_results)
+            .to_list(length=max_results)
+    )
+    
+    next_offset = offset + max_results if offset + max_results < total_results else ''
+    
     return files, next_offset, total_results
 
 # async def get_search2_results(query, file_type=None, max_results=10, offset=0, filter=False):
